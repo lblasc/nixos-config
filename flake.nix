6d7915957d0b4eac2bb35377cdb86a5a0200a1aa
Luka Blaskovic <lblasc@znode.net>
2021-11-14 17:34:42 +0100

    flake.nix, sops-nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs = {
-    sops-nix.url = github:Mic92/sops-nix;
     nixpkgs.url = github:NixOS/nixpkgs/master;
+    sops-nix.url = github:Mic92/sops-nix;
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, sops-nix }:
