b1d83d70decdd43ad308b3fb93d48e388c56fca2
Luka Blaskovic <lblasc@znode.net>
2024-11-24 11:32:46 +0100

    bump

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable-small;
     tvbeat-ssh.url = github:tvbeat/tvbeat-ssh;
     tvbeat-ssh.inputs.nixpkgs.follows = "nixpkgs";
   };
